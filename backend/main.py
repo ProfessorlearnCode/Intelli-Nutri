4e9444531a0e88d3e4237102d72135a79fa8b23c
Farzam Asad <141370482+ProfessorlearnCode@users.noreply.github.com>
2025-06-07 14:56:38 +0000

    Saving preferences from the form - dummy impl 📜

--- backend/main.py
+++ backend/main.py
@@ -9,14 +9,31 @@ Define all main routes here:
 Other files like llm_utils.py or db_utils.py will be imported and used here.
 """
 
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import db_utils as d
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def server_health():
     return "Backend is on 💯"
+
+@app.route("/save_preferences", methods=["POST"])
+def save_preferences():
+    try:
+        fetch = request.get_json()
+        preferences = fetch.get("preferences")
+        if not preferences:
+            return jsonify({"status" : 0,
+                            "message": "[🔴 ERROR] Error Occured while fetching the preferences - Client side"}), 400
+            
+        d.save_user_prefs(preferences=preferences)
+        return jsonify({"status" : 1}), 200
+    except Exception as e:
+        return jsonify({"status" : 0,
+                "message": "[🔴 ERROR] Error Occured while fetching and saving the preferences - Server side"}), 500
 
 
 
